eac8783110e8292fdf1b2b47f57c1be139fcb66e
Giorgi Abashidzee <abashidze.official@gmail.com>
2024-01-21 17:06:10 +0400

    Update

---
+++ src/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logos from './images/logos.png';
+import moon from './images/icon-moon.svg';
+
+function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-logo">
+                <img src={logos} className='logo' alt="Initiative Logo" />
+            </a>
+            <ul>
+                <li>
+                    <a href="Take action">Take action</a>
+                </li>
+                <li>
+                    <a href="News">News</a>
+                </li>
+                <li>
+                    <a href="Resurse libery">Resurse libery</a>
+                </li>
+            </ul>
+            <button className='them' >
+                <span className="moon-text">DARK</span>
+                <img src={moon} alt="moon" className="moon" />
+            </button>
+        </nav>
+    )
+}
+
+export default Navbar
