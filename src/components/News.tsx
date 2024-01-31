aab57d5dbf4ebe332fda4a9000d001b0343da368
Giorgi Abashidzee <abashidze.official@gmail.com>
2024-01-31 22:30:38 +0400

    Update

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -4,37 +4,21 @@ function News() {
     return (
         <div className='news-container'>
             <div className='news'>
-                <span className='news-title'>NEWS</span>
+                <span className='news-title'>სიახლეები</span>
             </div>
             <div className='news-line'></div>
             <div className='week-line'>
                 <div className='news-line1'>
-                    <span className='week-day'>Monday</span>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
+                    <span className='week-day'>სიახლე</span>
                 </div>
                 <div className='news-line1'>
-                    <span className='week-day'>Monday</span>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
+                    <span className='week-day'>სიახლე</span>
                 </div>
                 <div className='news-line1'>
-                    <span className='week-day'>Monday</span>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
+                    <span className='week-day'>სიახლე</span>
                 </div>
                 <div className='news-line1'>
-                    <span className='week-day'>Monday</span>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
-                    <div className='monday-line'></div>
+                    <span className='week-day'>სიახლე</span>
                 </div>
             </div>
 
--- src/components/Resurse.tsx
+++ src/components/Resurse.tsx
@@ -4,7 +4,14 @@ function Resurse() {
     return (
         <div className='resurse-container'>
             <div className='resurse'>
-                <span className='resurse-txt'>RESURSE LIBERY</span>
+                <span className='resurse-txt'>მულტიმედია</span>
+                <button className='download-btn'>
+                    <div className='download-pdf'>
+                        <span>
+                            ჩამოტვირთვა
+                        </span>
+                    </div>
+                </button>
             </div>
         </div>
     )
